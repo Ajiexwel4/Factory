dc9fb6e5973c80200aa46c8a23652a0c049d2da8
Ajiexwel4 <ajiexwel4@gmail.com>
2016-10-23 12:12:03 +0300

    Factory

---
+++ factory.rb
@@ -0,0 +1,44 @@
+# Factory insteed Struct
+class Factory
+  def self.new(*keys, &block)
+    Class.new do
+      class_eval(&block) if block_given?
+
+      attr_accessor(*keys)
+      define_method 'initialize' do |*values|
+        keys.each_with_index do |key, i|
+          instance_variable_set("@#{key}", values[i])
+        end
+      end
+
+      def [](arg)
+        arg = instance_variables[arg][1..-1] if arg.is_a? Numeric
+        instance_variable_get("@#{arg}")
+      end
+
+      def to_s
+        print "#<factory #{self.class}"
+        instance_variables.each do |var|
+          print " #{var[1..-1]}=" + '"' + instance_variable_get(var).to_s + '"'
+        end
+      end
+    end
+  end
+end
+
+# Tests
+puts 'Test 1'
+p User1 = Factory.new(:name, :address, :zip)
+p op = User1.new('Dave', '123 Main', 231)
+p op.name
+p op['name']
+p op[:name]
+p op[0]
+
+puts 'Test 2'
+User2 = Factory.new(:name, :address) do
+  def greeting
+    "Hello #{name}!"
+  end
+end
+p User2.new('Dave', '123 Main').greeting # => "Hello Dave!"
